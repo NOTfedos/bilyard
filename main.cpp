4c1c436f9e2be3bfd7756223f1912bc6c20ccad0
NOTfedos <mistkap@gmail.com>
2021-02-16 12:58:54 +0300

    working version v1.0

--- main.cpp
+++ main.cpp
@@ -1,11 +1,17 @@
 #include <iostream>
 #include <gmpxx.h>
+#include <ctime>
+#include <fstream>
+#include <iomanip>
 
 
 double PI = 3.141592653589793238462;
 
+using std::string;
 using std::cout;
 using std::endl;
+using std::ofstream;
+using std::setprecision;
 
 
 // Вычисление факториала целого числа
@@ -126,6 +132,12 @@ mpf_class get_len(mpf_class a){
 	mpf_class k;
 	k = get_k(a);
 
+	//cout << "Angle a = " << a << " k = " << k << endl;
+
+	if (k == 0){
+		return 1;
+	}
+
 
 	mpf_class x; x = 0;
 	mpf_class y; y = 0;
@@ -134,19 +146,15 @@ mpf_class get_len(mpf_class a){
 
 	mpf_class new_value; new_value = 0;
 
-	int coll_count = 0;  // техническая информация(для проверки работоспособности алгоритма)
-
 	// TODO: добавить вычисление пройденного расстояния
 	do{
-		cout << x << " " << y << endl;
+		//cout << x << " " << y << " " << k << endl;
 
-		int coll_count = 0;
+		int coll_count = 0;  // техническая информация(для проверки работоспособности алгоритма)
 
 		new_value = k * (0 - x) + y; 
-		if (!(y1 == y)){
-			cout << "round 1" << endl;
-			if ((0 < y1) && (y1 < 1)){
-				cout << "round 2" << endl;
+		if (!(new_value == y)){
+			if ((0 < new_value) && (new_value < 1)){
 				x1 = 0;
 				y1 = new_value;
 				coll_count++;
@@ -154,10 +162,8 @@ mpf_class get_len(mpf_class a){
 		}
 
 		new_value = k * (2 - x) + y;
-		if (!(y1 == y)){
-			cout << "round 1" << endl;
-			if ((0 < y1) && (y1 < 1)){
-				cout << "round 2" << endl;
+		if (!(new_value == y)){
+			if ((0 < new_value) && (new_value < 1)){
 				x1 = 2;
 				y1 = new_value;
 				coll_count++;
@@ -166,10 +172,8 @@ mpf_class get_len(mpf_class a){
 
 		// TODO: проверить деление на ноль
 		new_value = (0 - y) / k + x;
-		if (!(x1 == x)){
-			cout << "round 1" << endl;
-			if ((0 < x1) && (x1 < 2)){
-				cout << "round 2" << endl;
+		if (!(new_value == x)){
+			if ((0 < new_value) && (new_value < 2)){
 				x1 = new_value;
 				y1 = 0;
 				coll_count++;
@@ -177,10 +181,8 @@ mpf_class get_len(mpf_class a){
 		}
 
 		new_value = (1 - y) / k + x;
-		if (!(x1 == x)){
-			cout << "round 1" << endl;
-			if ((0 < x1) && (x1 < 2)){
-				cout << "round 2" << endl;
+		if (!(new_value == x)){
+			if ((0 < new_value) && (new_value < 2)){
 				x1 = new_value;
 				y1 = 1;
 				coll_count++;
@@ -198,7 +200,7 @@ mpf_class get_len(mpf_class a){
 
 		x = x1; y = y1;  // перемещаем бильярдный шар
 
-		cout << "ball set to x1 = " << x1 << ", y1 = " << y1 << endl;
+		//cout << "ball set to x1 = " << x1 << ", y1 = " << y1 << endl;
 
 
 	}while(loose(x, y));
@@ -209,18 +211,36 @@ mpf_class get_len(mpf_class a){
 
 int main ()
 {
-    int n = 100, dn = 1;
+    int n = 1000, dn = 2;
     
+    ofstream outf("output.txt");
+    if (!outf){
+    	return 1;
+    }
+
     mpf_class a, res;
+    mp_exp_t exp(-10);
     res = 0;
     a = 0;
 
+    int start = clock();  // засекаем время начала работы программы
+
     for(int iter_count = 1; iter_count <= 10; iter_count++){
+    	res = 0;
 	    for(int i = 0; i < n; i++){
-	    	a = i / n * PI / 2;  // Вычисление угла броска шара
+	    	a = i * PI / n / 2;  // Вычисление угла броска шара
 	    	res += get_len(a);  // Вычисление длины траектории
+	    	//cout << "Progress for " << iter_count << "/" << 10;
+	    	//cout << " " << i / n * 100 << "%" << endl;
 	    }
-	    cout << "| " << iter_count << " | " << res / n << endl;
+	    res = res / n;
+	    cout << "| " << iter_count << " | N = " << n << " | " << setprecision(20) << res << endl;
+	    outf << "| " << iter_count << " | N = " << n << " | " << setprecision(20) << res << endl;
 	    n *= dn;
 	}
+
+	int end = clock();
+	cout << "Calculating ended in " << (end - start) / CLOCKS_PER_SEC << " seconds" << endl;
+
+	return 0;
 }
--- work.bat
+++ work.bat
@@ -2,4 +2,5 @@ g++ main.cpp -lgmpxx -lgmp -o main.exe
 pause
 main.exe
 pause
+output.txt
 cls
